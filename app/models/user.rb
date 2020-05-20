38bb129421e3927ee0fd186eba28beb2c5abd077
Harry-KNIGHT <elliotharryk@gamil.com>
2020-05-20 23:50:41 +0200

    User mdp filtered with bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
 
-    validates :username, presence: true, length: {minimum: 3, maximum: 13}
-    validates :email, presence: true
+    validates :username, presence: { strict: true }, uniqueness: true, length: {minimum: 3, maximum: 13}
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, 
+               uniqueness: { case_sensitive: false },
+               format: { with: VALID_EMAIL_REGEX }
+    
+    has_secure_password
 
 end
